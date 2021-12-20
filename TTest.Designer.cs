e93fe20765009532fff37c87d2f234bb648c41d6
Zero-Point-Three Mach <45490638+Move2win@users.noreply.github.com>
2021-12-20 22:07:35 +0800

    Initial commit

---
+++ TTest.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Borderless
+{
+    partial class TTest
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customTrackBar1 = new Borderless.CustomTrackBar();
+            this.customTrackBar2 = new Borderless.CustomTrackBar();
+            this.customTrackBar4 = new Borderless.CustomTrackBar();
+            this.customTrackBar3 = new Borderless.CustomTrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customTrackBar1
+            // 
+            this.customTrackBar1.Location = new System.Drawing.Point(146, 112);
+            this.customTrackBar1.Name = "customTrackBar1";
+            this.customTrackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.customTrackBar1.Size = new System.Drawing.Size(45, 104);
+            this.customTrackBar1.TabIndex = 0;
+            this.customTrackBar1.Scroll += new System.EventHandler(this.customTrackBar1_Scroll);
+            this.customTrackBar1.Enter += new System.EventHandler(this.customTrackBar1_Enter);
+            this.customTrackBar1.Leave += new System.EventHandler(this.customTrackBar1_Leave);
+            // 
+            // customTrackBar2
+            // 
+            this.customTrackBar2.Location = new System.Drawing.Point(197, 112);
+            this.customTrackBar2.Name = "customTrackBar2";
+            this.customTrackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.customTrackBar2.Size = new System.Drawing.Size(45, 104);
+            this.customTrackBar2.TabIndex = 1;
+            this.customTrackBar2.Scroll += new System.EventHandler(this.customTrackBar2_Scroll);
+            this.customTrackBar2.Enter += new System.EventHandler(this.customTrackBar2_Enter);
+            this.customTrackBar2.Leave += new System.EventHandler(this.customTrackBar2_Leave);
+            // 
+            // customTrackBar4
+            // 
+            this.customTrackBar4.Location = new System.Drawing.Point(299, 112);
+            this.customTrackBar4.Name = "customTrackBar4";
+            this.customTrackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.customTrackBar4.Size = new System.Drawing.Size(45, 104);
+            this.customTrackBar4.TabIndex = 3;
+            this.customTrackBar4.Scroll += new System.EventHandler(this.customTrackBar4_Scroll);
+            this.customTrackBar4.Enter += new System.EventHandler(this.customTrackBar4_Enter);
+            this.customTrackBar4.Leave += new System.EventHandler(this.customTrackBar4_Leave);
+            // 
+            // customTrackBar3
+            // 
+            this.customTrackBar3.Location = new System.Drawing.Point(248, 112);
+            this.customTrackBar3.Name = "customTrackBar3";
+            this.customTrackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.customTrackBar3.Size = new System.Drawing.Size(45, 104);
+            this.customTrackBar3.TabIndex = 2;
+            this.customTrackBar3.Scroll += new System.EventHandler(this.customTrackBar3_Scroll);
+            this.customTrackBar3.Enter += new System.EventHandler(this.customTrackBar3_Enter);
+            this.customTrackBar3.Leave += new System.EventHandler(this.customTrackBar3_Leave);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(69, 25);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(297, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(394, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 21);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(433, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(33, 21);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(472, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(33, 21);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(511, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(33, 21);
+            this.textBox4.TabIndex = 12;
+            // 
+            // TTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 321);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.customTrackBar4);
+            this.Controls.Add(this.customTrackBar3);
+            this.Controls.Add(this.customTrackBar2);
+            this.Controls.Add(this.customTrackBar1);
+            this.Name = "TTest";
+            this.Text = "TTest";
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customTrackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomTrackBar customTrackBar1;
+        private CustomTrackBar customTrackBar2;
+        private CustomTrackBar customTrackBar4;
+        private CustomTrackBar customTrackBar3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ TTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Borderless
+{
+    public partial class TTest : Form
+    {
+        public TTest()
+        {
+            InitializeComponent();
+        }
+
+        #region 滑动条焦点指示器
+
+        private void customTrackBar1_Enter(object sender, EventArgs e)
+        {
+            label1.Show();
+        }
+
+        private void customTrackBar1_Leave(object sender, EventArgs e)
+        {
+            label1.Hide();
+        }
+
+        private void customTrackBar2_Enter(object sender, EventArgs e)
+        {
+            label2.Show();
+        }
+
+        private void customTrackBar2_Leave(object sender, EventArgs e)
+        {
+            label2.Hide();
+        }
+
+        private void customTrackBar3_Enter(object sender, EventArgs e)
+        {
+            label3.Show();
+        }
+
+        private void customTrackBar3_Leave(object sender, EventArgs e)
+        {
+            label3.Hide();
+        }
+
+        private void customTrackBar4_Enter(object sender, EventArgs e)
+        {
+            label4.Show();
+        }
+
+        private void customTrackBar4_Leave(object sender, EventArgs e)
+        {
+            label4.Hide();
+        }
+
+        #endregion
+
+        #region 滑动条数值指示器
+
+        private void customTrackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(customTrackBar1.Value);
+        }
+
+        private void customTrackBar2_Scroll(object sender, EventArgs e)
+        {
+            textBox2.Text = Convert.ToString(customTrackBar2.Value);
+        }
+
+        private void customTrackBar3_Scroll(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(customTrackBar3.Value);
+        }
+
+        private void customTrackBar4_Scroll(object sender, EventArgs e)
+        {
+            textBox4.Text = Convert.ToString(customTrackBar4.Value);
+        }
+
+        #endregion
+
+        //private void FocusIndicator()
+        //{
+        //    if (customTrackBar1.Focused)
+        //    {
+        //        label1.Show();
+        //    }
+        //    else if (customTrackBar2.Focused)
+        //    {
+        //        label2.Show();
+        //    }
+        //    else if (customTrackBar3.Focused)
+        //    {
+        //        label3.Show();
+        //    }
+        //    else if (customTrackBar4.Focused)
+        //    {
+        //        label4.Show();
+        //    }
+        //    else
+        //    {
+        //        label1.Hide();
+        //        label2.Hide();
+        //        label3.Hide();
+        //        label4.Hide();
+        //    }
+        //}
+
+    }
+}
